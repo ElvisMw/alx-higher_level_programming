09816edfbfedb24eff1e2e7c27696b57266b9ecc
ElvisMw <elvismwanthi@gmail.com>
2023-08-07 08:37:10 -0700

    Task 2

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-# This is a python script that prints exactly "Programming is like
-# building a multilingual puzzle followed" by a new line.
 print("Programming is like building a multilingual puzzle\n")
