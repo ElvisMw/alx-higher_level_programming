ddb039feea56459183254736f1b328a7dbefe055
root <root@DESKTOP-DMRT987>
2023-08-08 20:41:05 +0300

    5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+# A program that prints numbers from 0 to 99.
+# Numbers must be separated by ,, followed by a space
+# Numbers should be printed in ascending order, with two digits
+# The last number should be followed by a new line
+# You can only use no more than 2
+# print functions with string format
+# You can only use one loop in your code
+# You are not allowed to store numbers
+# or strings in a variable
+# You are not allowed to import any modul
+
+for num in range(100):
+    print(f"{num:02}", end=", " if num < 99 else "\n")
+
