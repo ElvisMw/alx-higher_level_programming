3fcaafad24d07fcf1bbcb84530e39c5a035e1024
root <root@DESKTOP-DMRT987>
2023-08-08 20:27:33 +0300

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 # A program that prints all numbers from 0 to 98
 # in decimal and in hexadecimal
 
-for number in range(0, 99):
-    print(f"{number} = 0x{number:x}")
+for numero in range(0, 99):
+    print("{} = {}".format(numero, hex(numero)))
