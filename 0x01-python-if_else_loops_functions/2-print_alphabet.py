b2d2a0835b5dab5ad2dd177352879110dbe4dc61
root <root@DESKTOP-DMRT987>
2023-08-08 18:59:07 +0300

    2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a program that prints the ASCII alphabet,
+# in lowercase, not followed by a new line.
+# Using only 1 loop, 1 print function with string format
+# not allowed to store characters in a variable
+# not allowed to import any module.
+
+for e in range(97, 123):
+    print(chr(e), end='')
