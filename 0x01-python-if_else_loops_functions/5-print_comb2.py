6d4f05ccef2aed25330d3c3eec2b8d78138bd5a5
root <root@DESKTOP-DMRT987>
2023-08-08 20:43:13 +0300

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 # A program that prints numbers from 0 to 99.
 # Numbers must be separated by ,, followed by a space
 # Numbers should be printed in ascending order, with two digits
