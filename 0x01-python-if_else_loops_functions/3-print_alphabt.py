03efab915edee1dd037ed251b9d6ec2a56243075
root <root@DESKTOP-DMRT987>
2023-08-08 20:12:58 +0300

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# a program that prints the ASCII alphabet,
+# in lowercase, not followed by a new line.
+# Using only 1 loop, 1 print function with string format
+# not allowed to store characters in a variable
+# not allowed to import any module.
+
+for e in range(97, 123):
+    if chr(e) != 'q' and chr(e) != 'e':
+        print("{}".format(chr(e)), end="")
