4db9e9fa7eb8ac1d791b4fb1737093afb5701f77
root <root@DESKTOP-DMRT987>
2023-08-09 19:11:56 +0300

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Prototype: def uppercase(str):
+# Only use no more than 2 print functions with string format
+# Only use one loop in your code
+# Not allowed to import any module
+# Not allowed to use str.upper() and str.isupper()
+
+def uppercase(input_str):
+    for char in input_str:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
