57c50998711736249097f641dd88d1e28dcfb0e5
root <root@DESKTOP-DMRT987>
2023-08-09 20:37:25 +0300

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+# This is function that prints last digit of number
+# and returns the value of the last digit
 
 def print_last_digit(number):
     number_1 = abs(number) % 10
     print(f"{number_1}", end="")
-    return(number_1)
+    return (number_1)
