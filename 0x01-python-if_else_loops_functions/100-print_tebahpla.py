bd4fc525db68e255e922f95a11ca6eeb94db79d3
root <root@DESKTOP-DMRT987>
2023-08-10 13:50:33 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+# This program prints theASCII alphabet in reverse order
+# While alternating lowercase and uppercase.
+# Using one loop, no sotrage of characters in variable
+# No importing any module
 
 el = 0
 for cha in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(cha - el)), end="")
     el = 32 if el == 0 else 0
-
-
