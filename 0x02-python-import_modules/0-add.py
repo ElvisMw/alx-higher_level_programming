6e8624a4a0332d1ee811d77f1e0936f2083c3d8e
root <root@DESKTOP-DMRT987>
2023-08-11 16:50:24 +0300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
 This program imports the function def add(a, b):
-from the file add_0.py and prints the result
+from the file add_0.py & prints the result
 of the addition 1 + 2 = 3
 It uses print function with string format to display integers
-You have to assign:
-the value 1 to a variable called a
-the value 2 to a variable called b
-The two variables are used as arguments when
+The code signs assign:
+1. the value 1 to a variable called a
+2. the value 2 to a variable called b
+- The two variables are used as arguments when
 calling the functions add and print
 - a and b must be defined in 2 different lines: a = 1 and another b = 2
 - The program prints: <a value> + <b value> = <add(a, b) value>
@@ -20,4 +20,4 @@ from add_0 import add
 if __name__ == '__main__':
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
