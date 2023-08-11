d1050deb410bcfcf41e82824b457c8ae0a5563e4
root <root@DESKTOP-DMRT987>
2023-08-11 17:03:29 +0300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # This program imports the function def add(a, b):
-# from the file add_0.py & prints the result
-# of the addition 1 + 2 = 3
-# Your code should not be executed when imported - by using __import__
 
 if __name__ == '__main__':
     from add_0 import add
