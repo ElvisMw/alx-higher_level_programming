13f34a5220b5d093b4164c96e08b1fb6bdba169f
root <root@DESKTOP-DMRT987>
2023-08-11 16:52:22 +0300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -16,8 +16,8 @@ followed with a new line
 - Not allowed to use * for importing or __import__
 Your code should not be executed when imported - by using __import__
 """
-from add_0 import add
 if __name__ == '__main__':
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
