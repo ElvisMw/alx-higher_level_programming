906330b4bf848dd8e3c6e9327492b34aa5542569
root <root@DESKTOP-DMRT987>
2023-08-14 18:06:35 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
-"""
-- This functionprints a matrix of integers using prototype
-def print_matrix_integer(matrix=[[]]).
-- not allowed to import any module and to
-cast integers into strings.
-- Assumption: List has integers only.
-"""
+
+# - This functionprints a matrix of integers using prototype
+# def print_matrix_integer(matrix=[[]]).
 
 
 def print_matrix_integer(matrix=[[]]):
