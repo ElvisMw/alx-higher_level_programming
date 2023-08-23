adef8e390fdd84e73843610de888f69911ea3376
root <root@DESKTOP-DMRT987>
2023-08-23 17:16:44 +0300

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# This function that replaces all occurrences of
+# an element by another in a new list.
+# Prototype is: def search_replace(my_list, search, replace):
+# 'my_list' is the initial list.
+# 'Search' is the element to replace in the list.
+# 'replace' is the new element.
+# Not allowed to import any module
+
+def search_replace(my_list, search, replace):
+    nu_list = list(map(lambda x: replace if x == search else x, my_list))
+    return nu_list
+
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+nu_list = search_replace(my_list, 2, 89)
+
+print(nu_list)
+print(my_list)
