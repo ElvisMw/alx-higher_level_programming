fc5c6e9334965f3563ba46a84b3f41331ee49c98
root <root@DESKTOP-DMRT987>
2023-08-30 14:41:47 +0300

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# This function raises a type exception.
+# The prototype is: def raise_exception():
+# Not allowed to import any module
+
+
+def raise_exception():
+    raise TypeError
