e9a1ce1ea757f1de364396deb5807dc49e8b9db3
root <root@DESKTOP-DMRT987>
2023-08-30 15:33:37 +0300

    100-

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# This function prints an integer.
+# The prototype is: def safe_print_integer_err(value):
+# value can be any type (integer, string, etc.)
+# The integer should be printed followed by a new line
+# Returns True if value has been correctly
+# printed (it means the value is an integer)
+# Otherwise, returns False and prints in
+# stderr the error precede by Exception:
+# try: / except: is used
+# "{:d}".format() to print as integer is used
+# Not allowed to use type()
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
