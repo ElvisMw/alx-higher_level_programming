b4f1231ccef5c211cb6c9c2752d2b4d8bd25d690
root <root@DESKTOP-DMRT987>
2023-08-30 15:25:04 +0300

    6-raise_exception_msg.py

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# This function raises a name exception with a message.
+# The prototype is: def raise_exception_msg(message=""):
+# Not allowed to import any module
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
