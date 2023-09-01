5fced122b7f425c1cabcba83ae2a9d9c97acd7bc
root <root@DESKTOP-DMRT987>
2023-09-01 22:57:48 +0300

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/python3
-"""This is an empty class Square that defines a square"""
+"""This is an empty class Square that defines a square."""
 
 
 class Square:
-    """Pass to indicate igonre the rest of  code"""
+    """
+    Pass to indicate igonre the rest of code.
+
+    Attributes:
+        No attributes for the above class.
+    """
     pass
