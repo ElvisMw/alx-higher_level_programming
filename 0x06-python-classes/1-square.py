4db0e1d2bcf18234e77b442decca17c783d44ced
root <root@DESKTOP-DMRT987>
2023-09-01 23:05:22 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""The belw  module defines a Square class."""
+
+
+class Square:
+    """
+    The Square class represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
