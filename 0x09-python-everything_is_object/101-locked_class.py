1a5a911a1c031369af9a374a3c078591280faaed
root <root@DESKTOP-DMRT987>
2023-09-06 21:35:19 +0300

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,6 +6,7 @@ class LockedClass:
     The LockedClass restricts the creation of new instance attributes.
 
     Attributes:
-        __slots__ (tuple of str): A tuple specifying the allowed instance attributes.
+        __slots__ (tuple of str): A tuple specifying
+        the allowed instance attributes.
     """
     __slots__ = ('first_name',)
