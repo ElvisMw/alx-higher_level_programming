2827a3eb7cef5cb81a49c9b69076b649718b6282
root <root@DESKTOP-DMRT987>
2023-09-06 09:56:02 +0300

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# This is an empty class Rectangle that defines a rectangle
-# Not allowed to import any module
+"""This is an empty class Rectangle that defines a rectangle
+Not allowed to import any module"""
+
 
 class Rectangle:
+    """Class for the rectsngle"""
     pass
