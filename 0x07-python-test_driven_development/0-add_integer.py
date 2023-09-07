c2d158ad2de2ac3a3d4fce40c8f18e04aa4e809c
root <root@DESKTOP-DMRT987>
2023-09-07 21:13:37 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,63 +1,20 @@
-#!/usr/bin/python3
-"""This function adds 2 integers """
-
-
 def add_integer(a, b=98):
     """
     Adds two integers.
 
     Args:
-        a (int or float): The first integer or float.
-        b (int or float, optional): The second integer or float. Defaults
-        to 98
+        a (int or float): The first number to be added.
+        b (int or float, optional): The second number to be added. Defaults to 98.
 
     Returns:
-        int: The addition of a and b as an integer.
+        int: The sum of the two input numbers, cast to an integer.
 
     Raises:
-        TypeError: If either a or b is not an integer or float.
-
-    Example:
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100, -2)
-        98
-        >>> add_integer(2)
-        100
-        >>> add_integer(100.3, -2)
-        98
-        >>> add_integer(4, "School")
-        Traceback (most recent call last):
-            ...
-        TypeError: b must be an integer
-        >>> add_integer(None)
-        Traceback (most recent call last):
-            ...
-        TypeError: a must be an integer
+        TypeError: If either `a` or `b` is not an integer or float.
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("b must be an integer or float")
 
-    a = int(a)
-    b = int(b)
-
-    return a + b
-
-
-""" Test cases"""
-
-if __name__ == "__main__":
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
+    return int(a) + int(b)
