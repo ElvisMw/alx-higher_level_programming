3057bb1d6792f6f2cf25041fee9044b64b7034af
root <root@DESKTOP-DMRT987>
2023-09-07 21:16:21 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
+"""This function add two numbers"""
+
 def add_integer(a, b=98):
     """
     Adds two integers.
 
     Args:
         a (int or float): The first number to be added.
-        b (int or float, optional): The second number to be added. Defaults to 98.
+        b (int or float, optional): The second number
+        to be added. Defaults to 98.
 
     Returns:
         int: The sum of the two input numbers, cast to an integer.
