98228e2b4598c15d32d17f40dd43495a642d7557
root <root@DESKTOP-DMRT987>
2023-09-11 19:52:21 +0300

    1-main.py

--- 0x0A-python-inheritance/tests/1-main.py
+++ 0x0A-python-inheritance/tests/1-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
@@ -9,28 +11,5 @@ my_list.append(3)
 my_list.append(5)
 
 print(my_list)
-"""
-Creates a MyList object and appends integers to it. Prints the original list.
-
-Example:
-    Output:
-    [1, 4, 2, 3, 5]
-"""
-
 my_list.print_sorted()
-"""
-Sorts and prints the MyList object in ascending order.
-
-Example:
-    Output:
-    [1, 2, 3, 4, 5]
-"""
-
 print(my_list)
-"""
-Prints the MyList object after sorting, which remains unchanged.
-
-Example:
-    Output:
-    [1, 2, 3, 4, 5]
-"""
