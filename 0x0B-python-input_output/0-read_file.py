4d40b9d6e4a2d036d59b03483d4c4118de37aff8
root <root@DESKTOP-DMRT987>
2023-09-12 22:50:15 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,8 @@ def read_file(filename=""):
     Read the contents of a text file and print them to stdout.
 
     Args:
-        filename (str): The name of the file to be read. Defaults to an empty string.
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
 
     Returns:
         None
@@ -15,6 +16,7 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line, end="")
+
 
 if __name__ == "__main__":
     read_file("my_file_0.txt")
