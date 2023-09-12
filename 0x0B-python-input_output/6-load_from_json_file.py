5eb0c25656f4f12c18c334664f1f5366bbfb891c
root <root@DESKTOP-DMRT987>
2023-09-12 23:41:58 +0300

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This function creates an Object from a “JSON file”""
+"""This function creates an Object from a “JSON file”"""
 import json
+
 
 def load_from_json_file(filename):
     """
