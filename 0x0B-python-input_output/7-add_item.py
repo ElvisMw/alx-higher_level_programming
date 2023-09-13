5a331039c68682293914102c0254441b4b910c0f
root <root@DESKTOP-DMRT987>
2023-09-13 19:06:12 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""Adds command line arguments to a list and saves it to a JSON file."""
+"""
+This script adds command line arguments to a Python list and
+saves them to a JSON file.
+
+Usage:
+- It loads the existing JSON file 'add_item.json' if it exists and
+initializes an empty list if not.
+- The script then extends the list with the command line arguments
+provided.
+- Finally, it saves the updated list to the 'add_item.json' file
+using the functions 'save_to_json_file' and
+'load_from_json_file' from the respective modules.
+
+Note: This script assumes that the 'save_to_json_file' and
+'load_from_json_file' functions exist and can handle JSON file operations
+"""
 
 import sys
-import os.path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
 
 if __name__ == "__main__":
-    """
-    This script adds command line arguments to a list and
-    saves it to a JSON file.
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    Usage:
-    - If the JSON file 'add_item.json' exists, it loads its
-    contents into a list.
-    - If the file does not exist, it initializes an empty list.
-    - Then, it extends the list with the command line arguments provided.
-    - Finally, it saves the updated list to the 'add_item.json' file.
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
 
-    This script depends on the 'save_to_json_file' and
-    'load_from_json_file' functions.
-    """
+    items.extend(sys.argv[1:])
 
-    json_file = "add_item.json"
-
-    if os.path.exists(json_file):
-        my_list = load_from_json_file(json_file)
-    else:
-        my_list = []
-
-    my_list.extend(sys.argv[1:])
-
-    save_to_json_file(my_list, json_file)
+    save_to_json_file(items, "add_item.json")
