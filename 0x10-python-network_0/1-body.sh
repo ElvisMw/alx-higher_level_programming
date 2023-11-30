47ddc393cf1d49104e32677c773e53bdd2680d60
ElvisMw <elvismwanthi@gmail.com>
2023-12-01 00:57:43 +0300

    web stack debugging

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,19 +1,3 @@
-#!/usr/bin/env bash
-
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send a GET request and display the body if the status code is 200
-body=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$body" -eq 200 ]; then
-    curl -s "$url"
-else
-    echo "Error: Received status code $body"
-fi
+#!/usr/bin/bash
+# This script takes a URL, sends a GET request, and displays the body of the response for a 200 status code
+curl -sL "$1" -w "%{http_code}" | grep -q 200 && curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/env bash
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send a DELETE request and display the body
-curl -sX DELETE "$url"
+#!/usr/bin/bash
+# This script sends a DELETE request to the provided URL and displays the body of the response
+curl -sX DELETE "$1"
