498e9d66b3b9ba409a072ba61556bd0bfae33360
ElvisMw <elvismwanthi@gmail.com>
2023-12-01 10:14:00 +0300

    web server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/bash
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
 
 # Get the URL from the command line argument
 url=$1
 
 # Use curl to send an OPTIONS request and display the Allow header
-curl -sI -X OPTIONS "$url" | grep -i "Allow" | cut -d " " -f 2-
+curl -sI -X OPTIONS "$url" | grep -i "Allow" | cut -d ":" -f 2- | cut -c 2-
