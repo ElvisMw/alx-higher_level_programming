0643b706bec67aeb1f6ea1f7c10ea877f3f7f771
ElvisMw <elvismwanthi@gmail.com>
2024-01-14 18:53:20 +0300

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,19 +3,23 @@
 This script lists all states from - database hbtn_0e_0_usa:
 It takes 3 arguments: mysql username, mysql password
 and database name (no argument validation needed)
-must use the module MySQLdb (import MySQLdb).
+must use the module MySQLdb (import MySQLdb)
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT name FROM states")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row[0])
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        cur.close()
+        db.close()
