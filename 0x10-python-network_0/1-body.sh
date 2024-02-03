8557d9ad1b32c7ef19486bbda18563bdcd31c12e
ElvisMw <elvismwanthi@gmail.com>
2024-02-03 19:34:47 +0300

    1-body.sh fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request, and displays the body of the response for a 200 status code
 
-response=$(curl -s -w "%{http_code}" "$1")
+response=$(curl -sL -w "%{http_code}" "$1")
 
 status_code=$(tail -n1 <<< "$response")
+body=$(sed '$d' <<< "$response")
 
 if [ "$status_code" -eq 200 ]; then
-    body=$(sed '$d' <<< "$response")
     echo "$body"
-else
-    echo "Unexpected status code: $status_code"
 fi
