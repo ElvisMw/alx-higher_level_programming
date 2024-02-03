3607b9f343bb93fe58ceca602438ae9081b59746
ElvisMw <elvismwanthi@gmail.com>
2024-02-03 22:50:29 +0300

    1 body.sh fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request, and displays the body of the response for a 200 status code
 
-response=$(curl -sL -w "%{http_code}" "$1")
+response=$(curl -sL -w "%{http_code}\n" "$1")
 
 status_code=$(tail -n1 <<< "$response")
 body=$(sed '$d' <<< "$response")
