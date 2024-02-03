c44a603c76f0f320dad9aa234528357f4c7ff193
ElvisMw <elvismwanthi@gmail.com>
2024-02-03 23:02:46 +0300

    1 body fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request to the URL using curl in silent mode, and displays the body of the response only for a 200 status code.
-
-curl -s -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+# script accepts  URL, sends a GET request,  displaybody of the response for a 200 status code
+curl -sL "$1"
