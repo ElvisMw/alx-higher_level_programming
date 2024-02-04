5df3869d88495bdac010d86ffb6dc58b33e2f759
ElvisMw <elvismwanthi@gmail.com>
2024-02-04 12:20:50 +0300

    3 error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Takes in a URL, sends a request, and displays the body of the response
-Handles urllib.error.HTTPError exceptions and prints the HTTP status code on error
+Handles urllib.error.HTTPError exceptions and prints he HTTP status code on error.
 """
 import sys
 import urllib.error
