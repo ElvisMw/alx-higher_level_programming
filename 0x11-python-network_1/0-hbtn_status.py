d29044344db467b4a075529e6037e20a652054c6
ElvisMw <elvismwanthi@gmail.com>
2024-02-04 11:17:13 +0300

    0 hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""A python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 
