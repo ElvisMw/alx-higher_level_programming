6d294a0ed9c7bc3a1aa7a7da1c40a6cd7f09139f
ElvisMw <elvismwanthi@gmail.com>
2024-02-12 16:33:20 +0300

    6 post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a
-POST request to the passed URL with the email as a
-parameter, and displays the body of the response
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
 using the requests package.
 """
 
@@ -16,4 +15,11 @@ if __name__ == "__main__":
     payload = {'email': email}
     response = requests.post(url, data=payload)
 
-    print("Your email is:", response.text)
+    try:
+        json_response = response.json()
+        if 'Your email is' in json_response:
+            print(json_response['Your email is'])
+        else:
+            print(json_response)
+    except ValueError:
+        print(response.text)
