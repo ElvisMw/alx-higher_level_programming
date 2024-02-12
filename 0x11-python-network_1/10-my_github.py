9729a719638b37d6a58293d40670cbf4750b4c23
ElvisMw <elvismwanthi@gmail.com>
2024-02-12 16:35:14 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
+
+Uses Basic Authentication with a personal access token as password
+to access your information (only read:user permission is needed).
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        json_response = response.json()
+        if 'id' in json_response:
+            print(json_response['id'])
+        else:
+            print(None)
+    except ValueError:
+        print(None)
