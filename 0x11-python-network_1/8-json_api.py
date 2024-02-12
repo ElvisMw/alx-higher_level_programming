d89223e788fb5b8c3cbbc48fad884c6ebb7fbd67
ElvisMw <elvismwanthi@gmail.com>
2024-02-12 16:28:55 +0300

    python network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status using
-the requests package.
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+The letter must be sent in the variable q.
+If no argument is given, set q="".
+If the response body is properly JSON formatted and not empty, display the id
+and name like this: [<id>] <name>.
+Otherwise:
+Display Not a valid JSON if the JSON is invalid.
+Display No result if the JSON is empty.
+Uses the package requests and sys.
 """
 
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
 
-    print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    payload = {'q': q}
+    response = requests.post(url, data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
